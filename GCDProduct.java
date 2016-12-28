bc92ac0e7496bac4c4879505662cc4be5525024b
Prudhvi Rajkumar <prudhvirajnitjsr@gmail.com>
2016-12-28 12:20:10 +0530

    Count of pairs with given GCD
    This is a very very nice problem from Hackerrank. This is application of Mobius function. But, I found a solution which doesn't use the Mobius function. I tried this problem by appliying Mobius function, but could only get partial score. By going through tester's solution, I could find elegant solution. This is a very nice learning.

---
+++ GCDProduct.java
@@ -0,0 +1,39 @@
+//PROBLEM : https://www.hackerrank.com/contests/w3/challenges/gcd-product
+
+import java.io.*;
+import java.util.*;
+
+public class GCDProduct {
+
+    static final int MAX = 15000000,MOD = 1000000007;
+    
+    static long pow(long x , long y){
+        if(y==0)return 1;
+        long ans = pow(x,y/2);
+        ans = (ans*ans)%MOD;
+        if(y%2==1)ans = (ans*x)%MOD;
+        return ans;
+    }
+             
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        InputReader ir = new InputReader();
+        long N = ir.readInt();
+        long M = ir.readInt();        
+        long F[] = new long[MAX+1];
+        for(int i = 1; i <= MAX; i++){
+            F[i] = (1l * (N / i) * (M / i)) ;
+        }
+        for(int i = MAX; i >= 1; i--)
+            for(int j = i + i; j <= MAX; j += i) {
+                F[i] -= F[j];
+            }
+    
+        int e = (int)Math.min(N, M);
+        long ans = 1;
+        for(int i=2;i<=e;i++){
+            ans = (ans * pow(i,F[i]))%MOD;
+        }
+        System.out.println(ans);        
+    }
+}
