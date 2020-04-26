964e1931cce598da3972da73d32721e5c4ff4717
Prudvi Rajkumar <prudhvirajnitjsr@gmail.com>
2020-04-26 21:39:51 +0530

    Added Private and Public methods

--- AbstractSegmentTree.java
+++ AbstractSegmentTree.java
@@ -1,17 +1,20 @@
 import java.util.Arrays;
 
 abstract class AbstractSegmentTree {
-    public int tree[];
-    int n;
+    private int tree[];
+    private int n;
 
-    public abstract int getDefaultValue();
+    public abstract int getIdentityValue();
+
+    public abstract int associativeFunction(int leftVal, int rightVal);
 
     public AbstractSegmentTree(int n) {
         int s = (int) Math.pow(2, Math.ceil(Math.log(n + 1) / Math.log(2)) + 1) - 1;
         tree = new int[s];
+        this.n = n;
     }
 
-    void updateSegmentTree(int u, int val, int left, int right, int current) {
+    private void update(int u, int val, int left, int right, int current) {
         if (u < left || u > right || left > right) {
             return;
         }
@@ -21,40 +24,48 @@ abstract class AbstractSegmentTree {
             return;
         }
         int mid = (left + right) >> 1;
-
-        updateSegmentTree(u, val, left, mid, 2 * current + 1);
-        updateSegmentTree(u, val, mid + 1, right, 2 * current + 2);
+        update(u, val, left, mid, 2 * current + 1);
+        update(u, val, mid + 1, right, 2 * current + 2);
         tree[current] = associativeFunction(tree[2 * current + 1], tree[2 * current + 2]);
     }
 
-    int getVal(int qLeft, int qRight, int left, int right, int current) {
-        if (qRight < left || qLeft > right || left>right) {
-            return getDefaultValue();
+    public void update(int pos, int value) {
+        update(pos, value, 0, n - 1, 0);
+    }
+
+    private int get(int qLeft, int qRight, int left, int right, int current) {
+        if (qRight < left || qLeft > right || left > right) {
+            return getIdentityValue();
         }
         if (qLeft <= left && qRight >= right) {
             return tree[current];
         }
         int mid = (left + right) >> 1;
-        return associativeFunction(getVal(qLeft, qRight, left, mid, 2 * current + 1), getVal(qLeft, qRight, mid + 1, right, 2 * current + 2));
+        return associativeFunction(get(qLeft, qRight, left, mid, 2 * current + 1), get(qLeft, qRight, mid + 1, right, 2 * current + 2));
     }
 
-    public abstract int associativeFunction(int leftVal, int rightVal);
+    public int get(int qLeft, int qRight) {
+        return get(qLeft, qRight, 0, n - 1, 0);
+    }
 
+    public void printTree() {
+        System.out.println(Arrays.toString(tree));
+    }
 }
 
-class MaxSegmentTree extends AbstractSegmentTree{
+class MaxSegmentTree extends AbstractSegmentTree {
 
     public MaxSegmentTree(int n) {
         super(n);
     }
 
     @Override
-    public int getDefaultValue() {
+    public int getIdentityValue() {
         return 0;
     }
 
     @Override
     public int associativeFunction(int leftVal, int rightVal) {
-        return Math.max(leftVal,rightVal);
+        return Math.max(leftVal, rightVal);
     }
 }
