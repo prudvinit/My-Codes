c44c83be24d7915371890a3234eec89534cb96a6
prudvinit <prudhvirajnitjsr@gmail.com>
2021-09-02 08:37:25 +0530

    Compression and Decompression #Google

---
+++ java/src/CompressionAndDecompression.java
@@ -0,0 +1,48 @@
+//https://techdevguide.withgoogle.com/resources/former-interview-question-compression-and-decompression/#!
+package src;
+
+public class CompressionAndDecompression {
+    static String compress(String s){
+        if(s.length()==0)
+            return s;
+        boolean startsWithDigit = Character.isDigit(s.charAt(0));
+        if(startsWithDigit){
+            int num = 0;
+            int ind = 0;
+            while (s.charAt(ind)!='['){
+                num = 10*num + s.charAt(ind)-48;
+                ind++;
+            }
+            int left = ind;
+            int lc = 1;
+            int right = ind+1;
+            for(int i=left+1;i<s.length();i++){
+                if(s.charAt(i)=='['){
+                    lc++;
+                }
+                else if(s.charAt(i)==']'){
+                    lc--;
+                    if(lc==0){
+                        right = i;
+                        break;
+                    }
+                }
+            }
+            String rec = compress(s.substring(left+1,right));
+            String ans = "";
+            for(int i=0;i<num;i++){
+                ans = ans + rec;
+            }
+            return ans + compress(s.substring(right+1));
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        String s = "3[abc]4[ab]c";
+        System.out.println(s+" : "+compress(s));
+        s = "10[a]";
+        System.out.println(s+" : "+compress(s));
+        s = "2[3[a]b]";
+        System.out.println(s+" : "+compress(s));
+    }
+}
--- java/src/MaximumRectangleAreaInHistogram.java
+++ java/src/MaximumRectangleAreaInHistogram.java
@@ -1,3 +1,4 @@
+//https://practice.geeksforgeeks.org/problems/maximum-rectangular-area-in-a-histogram-1587115620/1
 package src;
 
 import java.util.Arrays;
